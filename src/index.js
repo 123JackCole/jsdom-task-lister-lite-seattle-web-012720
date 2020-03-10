b3cfe8d66a2fe105c8705eb8986b1d8fa7aea8b0
123JackCole <123jack.cole@gmail.com>
2020-03-10 11:53:08 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+function addTask(task) {
+  let newTask = document.createElement("li");
+  newTask.textContent = task;
+  document.querySelector("ul").appendChild(newTask);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskToAdd = event.target["new-task-description"].value;
+    if (taskToAdd !== "") {
+      addTask(taskToAdd);
+
+      // ?
+      let newButton = document.createElement("button");
+      newButton.textContent = "x";
+      newButton.type = "delete";
+      document.querySelector("ul").appendChild(newButton);
+
+    }
+  });
+
+  // ?
+  form.addEventListener("delete", function(event) {
+    event.preventDefault();
+    const taskToDelete = event.target["new-task-description"].value;
+    document.querySelector("ul").removeChild(taskToDelete);
+    // .parentNode.removeChild() ;
+
+  });
+
 });
